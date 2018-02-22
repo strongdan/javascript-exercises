e81f010f48b3758f961c9efa9eb410796416f687
Strong <BROCD8S@labor.ak>
2018-02-22 14:27:43 -0900

    complete through snakeCase

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,25 @@
-var caesar = function() {
+var caesar = function(string, factor) {
+  let splitString = string.split('');
+  let newString = [];
+  // iterate over string characters
+  for (let i = 0; i < string.length; i++) {
+    // check if character is a space or punctuation
+    String punctuations = "' '.,:;!@#$%^&*()-";
+    if (punctuations.contains(string[i])) {
+      // push non-word characters and spaces to newString
+      newString.push(string[i]);
+    } else {
+        // if not, convert to character code and add or subtract factor
+        // account for uppercase
+        // account for end of alphabet
+        numeric = string[i].charCodeAt(0) + factor;
+        newChar = String.fromCharCode(numeric);
+        newString.push(newChar);
+    }
 
+  }
+  // return string with characters offset by factor
+  return newString.join('');
 }
 
 module.exports = caesar
