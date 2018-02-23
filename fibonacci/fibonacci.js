6f496e358cf544f31ea7cc675fb2b97e323ae0ed
Daniel Strong <danielstrong@mac.com>
2018-02-23 09:59:26 -0900

    solved fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-var fibonacci = function() {
-
+var fibonacci = function(n) {
+  let currentNum;
+  let numList = [0,1];
+  if (n > 0) {
+    for (let i = 2; i <= n; i++) {
+      currentNum = numList[i - 1] + numList[i - 2];
+      numList.push(currentNum);
+    }
+  } else {
+    return 'OOPS';
+  }
+  return numList[n];
 }
 
 module.exports = fibonacci
--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,9 +1,24 @@
-function translate() {
-	// body...
+function translate(string) {
+	let splitString = string.split(' ');
+	let newString = [];
+	for (let i = 0; i < splitString.length; i++) {
+		if (splitString[i].substring(0,1) === 'a' || 'e' || 'i' || 'o' || 'u') {
+			pigWord = `${splitString[i]}way`;
+			newString.push(pigWord);
+		} else if (splitString[i].substring(0,2) === 'ch' || 'sh' || 'sm' || 'st' || 'th' || 'st' || 'gl') {
+			pigWord = `${splitString[i].substring(2,splitString.length)}${splitString[i].substring(0,2)}ay`;
+			newString.push(pigWord);
+		} else if (splitString[i].substring(0,1) !== 'a' || 'e' || 'i' || 'o' || 'u') {
+			pigWord = `${splitString[i].substring(1,splitString.length)}${splitString[i].substring(0,1)}ay`;
+			newString.push(pigWord);
+		} else {
+			return 'Error';
+		}
+	}
+	return newString.join(' ');
 }
 
 
 module.exports = {
 	translate
 }
-
